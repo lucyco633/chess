704f69864d98536df398cd107456f6bee37c929b
Lucy Collins <lucyco7@byu.edu>
2024-09-17 12:48:17 -0600

    king moves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * Represents a single chess piece
@@ -42,21 +43,7 @@ public class ChessPiece {
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        switch (type) {
-            case KING:
-                return PieceType.KING;
-            case QUEEN:
-                return PieceType.QUEEN;
-            case BISHOP:
-                return PieceType.BISHOP;
-            case KNIGHT:
-                return PieceType.KNIGHT;
-            case ROOK:
-                return PieceType.ROOK;
-            case PAWN:
-                return PieceType.PAWN;
-        }
-        //return statement?
+        return type;
     }
 
     /**
@@ -67,9 +54,109 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        switch (getPieceType()) {
+        Collection<ChessMove> valid_moves = new Collection<ChessMove>() {
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean contains(Object o) {
+                return false;
+            }
+
+            @Override
+            public Iterator<ChessMove> iterator() {
+                return null;
+            }
+
+            @Override
+            public Object[] toArray() {
+                return new Object[0];
+            }
+
+            @Override
+            public <T> T[] toArray(T[] a) {
+                return null;
+            }
+
+            @Override
+            public boolean add(ChessMove chessMove) {
+                return false;
+            }
+
+            @Override
+            public boolean remove(Object o) {
+                return false;
+            }
+
+            @Override
+            public boolean containsAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean addAll(Collection<? extends ChessMove> c) {
+                return false;
+            }
+
+            @Override
+            public boolean removeAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public boolean retainAll(Collection<?> c) {
+                return false;
+            }
+
+            @Override
+            public void clear() {
+
+            }
+        };
+        switch (type) {
             case KING:
-                if (board.getPiece(new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn())) == null)
+                if (board.getPiece(new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn())) == null){
+                    valid_moves.add(new ChessMove(myPosition, new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn()), type));
+                }
+                if (board.getPiece(new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn() + 1)) == null){
+                    valid_moves.add(new ChessMove(myPosition, new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn() + 1), type));
+                }
+                if (board.getPiece(new ChessPosition(myPosition.getRow(), myPosition.getColumn() + 1)) == null){
+                    valid_moves.add(new ChessMove(myPosition, new ChessPosition(myPosition.getRow(), myPosition.getColumn() + 1), type));
+                }
+                if (board.getPiece(new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn() + 1)) == null){
+                    valid_moves.add(new ChessMove(myPosition, new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn() + 1), type));
+                }
+                if (board.getPiece(new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn())) == null){
+                    valid_moves.add(new ChessMove(myPosition, new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn()), type));
+                }
+                if (board.getPiece(new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn() - 1)) == null){
+                    valid_moves.add(new ChessMove(myPosition, new ChessPosition(myPosition.getRow() - 1, myPosition.getColumn() - 1), type));
+                }
+                if (board.getPiece(new ChessPosition(myPosition.getRow(), myPosition.getColumn() - 1)) == null){
+                    valid_moves.add(new ChessMove(myPosition, new ChessPosition(myPosition.getRow(), myPosition.getColumn() - 1), type));
+                }
+                if (board.getPiece(new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn() - 1)) == null){
+                    valid_moves.add(new ChessMove(myPosition, new ChessPosition(myPosition.getRow() + 1, myPosition.getColumn() - 1), type));
+                }
+            case QUEEN:
+                break;
+            case BISHOP:
+                break;
+            case KNIGHT:
+                break;
+            case ROOK:
+                break;
+            case PAWN:
+                break;
         }
+        return valid_moves;
     }
 }
