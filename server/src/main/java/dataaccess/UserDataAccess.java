01826f74e77f8e18012cb00ad8175bf2624f2cae
Lucy Collins <lucyco7@byu.edu>
2024-10-17 18:46:41 -0600

    working on interfaces

--- server/src/main/java/dataaccess/UserDataAccess.java
+++ server/src/main/java/dataaccess/UserDataAccess.java
@@ -2,13 +2,14 @@ package dataaccess;
 import chess.ChessGame;
 import dataaccess.DataAccessException;
 //create model class with record for User? Where?
-//import model.Pet;
+import model.UserData;
 
-import java.util.ArrayList;
-import java.util.Collection;
 
 public interface UserDataAccess {
-    //public Collection<ChessGame> gamesList = new ArrayList<>();
+    UserData getUser(String username) throws DataAccessException;
+
+    UserData createUser(UserData user) throws DataAccessException;
+
 
 
 }
