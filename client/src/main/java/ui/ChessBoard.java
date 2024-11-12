8f9aefb8176dd190719fceed53adb15abc1cce18
Lucy Collins <lucyco7@byu.edu>
2024-11-12 10:02:57 -0700

    created ui.chessboard

---
+++ client/src/main/java/ui/ChessBoard.java
@@ -0,0 +1,16 @@
+package ui;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+
+import static ui.EscapeSequences.*;
+
+public class ChessBoard {
+
+    // Board dimensions
+    private static final int BOARD_SIZE_IN_SQUARES = 8;
+    private static final int SQUARE_SIZE_IN_PADDED_CHARS = 3;
+    private static final int LINE_WIDTH_IN_PADDED_CHARS = 1;
+
+}
